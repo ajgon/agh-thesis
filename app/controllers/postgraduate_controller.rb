c929a2c9ff0210c7dd7ef417bcdf21d9b7949662
Igor Rzegocki <ajgon@irgon.com>
2008-03-23 22:02:53 +0000

    Static deanery pages

---
+++ app/controllers/postgraduate_controller.rb
@@ -0,0 +1,16 @@
+class PostgraduateController < ApplicationController
+  def index
+  end
+  
+  def regulations
+  end
+  
+  def schedule
+  end
+  
+  def program
+  end
+  
+  def sked
+  end
+end
---
+++ app/helpers/postgraduate_helper.rb
@@ -0,0 +1,2 @@
+module PostgraduateHelper
+end
