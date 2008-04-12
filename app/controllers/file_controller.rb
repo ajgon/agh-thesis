a4d9b4fe8fd79161176682eb70d7b1f06dbcd76c
Igor Rzegocki <ajgon@irgon.com>
2008-04-12 16:09:58 +0000

    Added file permissions for students

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -2,10 +2,9 @@ class FileController < ApplicationController
   def download
     file_id = IdEncoder.decode(params[:id])
     if file_id and UploadedFile.exists?(file_id) and File.exists?(file = RAILS_ROOT + '/files/' + UploadedFile.find(file_id).filename)
-      if UploadedFile.is_public? file_id
+      if UploadedFile.is_public? file_id or (@logged_user and UploadedFile.can_user_get_file?(@logged_user.id, file_id))
         send_file file
       else
-        # TODO rozpatrywanie uprawnien
         render :template => 'file/access_denied'
       end
     else
--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -7,16 +7,36 @@ class UploadedFile < ActiveRecord::Base
     GroupsUploadedFile.find(:first, :conditions => ['uploaded_file_id = ?', file_id]).nil?
   end
   
-  def self.find_materials_for_user(id, kind = '', include_non_logged = true)
+  def self.find_materials_for_user(id, kind = '', include_all_logged = true)
     unless kind.nil? or kind.empty?
       kind = " AND kind = '#{kind.to_s}'"
     end
-    user_groups = []
-    user_groups = [1] if include_non_logged
-    GroupsUser.find(:all, :conditions => ['user_id = ?', id]).each {|groups_user| user_groups.push groups_user.group_id}
+    user_groups = self.acquire_groups id
+    user_groups.push(1) if include_all_logged
     if user_groups.empty?
       return []
     end
     return UploadedFile.find(:all, :include => [:groups_uploaded_files, :subject, :user], :conditions => "groups_uploaded_files.group_id IN (#{user_groups.join(', ')})#{kind}", :order => 'subject_id')
   end
+  
+  def self.can_user_get_file? user_id, file_id
+    user_groups = self.acquire_groups user_id
+    user_groups.push(1)
+    if self.is_public? file_id
+      return true
+    end
+    allowed = false
+    GroupsUploadedFile.find(:all, :conditions => ['uploaded_file_id = ?', file_id]).each do |groups|
+      allowed = true if user_groups.include? groups.group_id.to_i
+    end
+    return allowed
+  end
+  
+  private
+  def self.acquire_groups id
+    user_groups = []
+    GroupsUser.find(:all, :conditions => ['user_id = ?', id]).each {|groups_user| user_groups.push groups_user.group_id}
+    return user_groups
+  end
+  
 end
