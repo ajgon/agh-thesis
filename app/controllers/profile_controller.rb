63ce105d99e2f8f67a72a2211ce6d075ffbe0d64
Igor Rzegocki <ajgon@irgon.com>
2008-04-15 20:51:40 +0000

    Students and profiles views fixes and updates

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,10 +1,14 @@
 class ProfileController < ApplicationController
   def method_missing login
-    @profile = User.find(:first, :conditions => ['login = ?', login])
-    @news = News.find(:all, :conditions => ['user_id = ?', @profile.id], :limit => 5, :order => 'date DESC')
-    @files = UploadedFile.find(:all, :conditions => ['user_id = ? and uploaded_files.kind = ?', @profile.id, 'material'], :include => [:subject, :user], :limit => 5, :order => 'date DESC')
-    @grades = UploadedFile.find(:all, :conditions => ['user_id = ? and uploaded_files.kind = ?', @profile.id, 'grade'], :include => [:subject, :user], :limit => 5, :order => 'date DESC')
-    @subjects = UploadedFile.find(:all, :include => [:user, :subject], :conditions => ['users.id = ?', @profile.id], :group => 'subjects.id')
-    render :template => 'profile/index'
+    if @logged_user
+      @profile = User.find(:first, :conditions => ['login = ?', login])
+      @news = News.find(:all, :conditions => ['user_id = ?', @profile.id], :limit => 5, :order => 'date DESC')
+      @files = UploadedFile.find(:all, :conditions => ['user_id = ? and uploaded_files.kind = ?', @profile.id, 'material'], :include => [:subject, :user], :limit => 5, :order => 'date DESC')
+      @grades = UploadedFile.find(:all, :conditions => ['user_id = ? and uploaded_files.kind = ?', @profile.id, 'grade'], :include => [:subject, :user], :limit => 5, :order => 'date DESC')
+      @subjects = UploadedFile.find(:all, :include => [:user, :subject], :conditions => ['users.id = ?', @profile.id], :group => 'subjects.id')
+      render :template => 'profile/index'
+    else
+      render :template => 'signin/signin'
+    end
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,67 @@ class StudentsController < ApplicationController
   
   def index
     if @logged_user
+      @specialities = Speciality.find(:all).collect {|i| [i.head, IdEncoder.encode(i.id)]}
+      @sort = [
+        ['Nazwisko', 'lastname'],
+        ['Imię', 'firstname'],
+        ['Login', 'login'],
+        ['Rok', 'year'],
+        ['Adres E-mail', 'email'],
+        ['Nr telefony', 'cell'],
+        ['Adres strony WWW', 'www_page'],
+        ['Opis strony WWW', 'www_description'],
+        ['Numer Gadu Gadu', 'gadu_gadu'],
+        ['Numer ICQ', 'icq']
+      ]
+      @sort.push ['Numer indeksu', 'sindex'] if @logged_user.is_lecturer?
+    else
+      render :template => 'signin/signin'
+    end
+  end
+  
+  def results
+    if @logged_user
+      if request.post?
+        criteria = params[:criteria].clone
+        criteria.delete 'sort'
+        criteria.delete 'year'
+        criteria.delete 'speciality_id' if criteria[:speciality_id].nil? or IdEncoder.decode(criteria[:speciality_id]) == 1
+        criteria.each_pair do |key, value|
+          criteria.delete key if value.empty?
+        end
+        unless criteria.nil? or criteria.empty?
+          criteria[:speciality_id] = IdEncoder.decode(criteria[:speciality_id]) if criteria[:speciality_id]
+          conditions = [criteria.keys.join(' = ? AND ') + ' = ?'] + criteria.values
+          unless params[:criteria][:year].nil? or params[:criteria][:year].empty?
+            year = 'K%' + ((UsersStudent.find(:first, :order => 'sgroup DESC').sgroup[1..2].to_i + 1) - params[:criteria][:year].to_i).to_s + '_'
+            conditions[0] += ' AND sgroup LIKE ?'
+            conditions.push year
+          end
+          params[:criteria][:sort] = 'lastname' unless params[:criteria][:sort]
+          params[:criteria][:sort] = 'sgroup DESC' if params[:criteria][:sort] == 'year'
+          @results =  User.find(:all, :joins => :users_student, :conditions => conditions, :order => params[:criteria][:sort])
+          @fields = (params[:results].delete_if {|key, value| value.to_i == 0}).keys
+          @fields = ['login', 'firstname', 'lastname'] if @fields.empty?
+          @fields = ['login', 'firstname', 'lastname', 'email', 'sgroup', 'gadu_gadu', 'cell', 'icq', 'www_page', 'www_description'] & @fields
+          @fields_hash = {
+            'login' => 'Login',
+            'firstname' => 'Imię',
+            'lastname' => 'Nazwisko',
+            'email' => 'E-mail',
+            'sgroup' => 'Kod Grupy',
+            'gadu_gadu' => 'Gadu Gadu',
+            'cell' => 'Nr Telefonu',
+            'icq' => 'ICQ',
+            'www_page' => 'Adres WWW',
+            'www_description' => 'Opis WWW'
+          }
+        else
+          redirect_to :controller => 'students', :action => 'index'
+        end
+      else
+        redirect_to :controller => 'students', :action => 'index'
+      end
     else
       render :template => 'signin/signin'
     end
